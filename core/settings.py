da352035141ec25d51cf86691e931f01a4ff2576
Thainan Sousa <thainansousa7@gmail.com>
2023-07-22 12:32:58 -0300

    criado tela de login

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'alunos',
     'professores',
     'treinos',
-    'financeiro'
+    'financeiro',
+    'login'
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('login.urls')),
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('usuario/', include('usuarios.urls')),
