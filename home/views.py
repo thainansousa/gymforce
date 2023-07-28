66e77df1e143f5830bc089eb965bcb2ee39c69aa
Thainan Sousa <thainansousa7@gmail.com>
2023-07-27 21:31:50 -0300

    criado dashboard

--- home/views.py
+++ home/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+from alunos.models import Aluno
+
 def home(request):
-    return render(request, 'home.html')
+
+    alunos = Aluno.objects.all()
+
+    alunosAtivos = Aluno.objects.filter(status=True)
+
+    mensalidadesTotal = 0
+
+    for aluno in alunos:
+        if aluno.status == True:
+            mensalidadesTotal += aluno.mensalidade.valor
+
+    return render(request, 'home.html', {
+        'mensalidadesTotal': mensalidadesTotal, 
+        'alunosTotal': len(alunos),
+        'alunosAtivos': len(alunosAtivos)})
