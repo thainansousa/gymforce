afc8c85a13642dd3be9a1835f4f0942a70d02e03
Thainan Sousa <thainansousa7@gmail.com>
2023-07-30 16:29:26 -0300

    refactor professores models, adicionado choices status

--- professores/models.py
+++ professores/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class Professor(models.Model):
+
+    choices_status = (
+        (True, 'Ativo'),
+        (False, 'Inativo')
+    )
+
     nome = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     telefone = models.CharField(max_length=25)
     cpf = models.CharField(max_length=25)
     registro = models.CharField(max_length=50)
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=True, choices=choices_status)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
