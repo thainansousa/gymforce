fa9b3592568d95bd2bdd2b0bb51426a9d7ac7130
Thainan Sousa <thainansousa7@gmail.com>
2023-07-30 16:26:54 -0300

    refactor financeiro models, adicionado choices status

--- financeiro/models.py
+++ financeiro/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Mensalidade(models.Model):
+
+    choices_status = (
+        (True, 'Ativo'),
+        (False, 'Inativo')
+    )
+
     nome = models.CharField(max_length=255)
     valor = models.FloatField()
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=True, choices=choices_status)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
