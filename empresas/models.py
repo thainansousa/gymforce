a3ac262ffae0e9ea5ebddcfb32aa55c44f4d9101
Thainan Sousa <thainansousa7@gmail.com>
2023-07-30 16:24:45 -0300

    refactor empresas models, adicionado choices para o status

--- empresas/models.py
+++ empresas/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 class Empresa(models.Model):
 
+    choices_status = (
+        (True, 'Ativo'),
+        (False, 'Inativo')
+    )
+
     razaoSocial = models.CharField(max_length=255)
     fantasia = models.CharField(max_length=255)
     cnpj = models.CharField(max_length=100)
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=True, choices=choices_status)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
