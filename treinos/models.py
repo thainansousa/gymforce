8bcba7402dcdd3c1206d0b42603c0f84a869d3b9
Thainan Sousa <thainansousa7@gmail.com>
2023-07-30 16:29:45 -0300

    refactor treinos models, adicionado choices status

--- treinos/models.py
+++ treinos/models.py
@@ -24,12 +24,17 @@ class Treino_Aluno(models.Model):
         ('DOM', 'DOMINGO'),
     )
 
+    choices_status = (
+        (True, 'Ativo'),
+        (False, 'Inativo')
+    )
+
     treino = models.ForeignKey(Treino, on_delete=models.DO_NOTHING)
     treino_series = models.IntegerField(default=0)
     treino_qtd = models.IntegerField(default=0)
     treino_dia = models.CharField(max_length=3, choices=choice_diasDaSemana)
     aluno = models.ForeignKey(Aluno, on_delete=models.DO_NOTHING)
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=True, choices=choices_status)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
