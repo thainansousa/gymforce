4a75de645e319db98cc3f06c114ea3e689ecbe21
Thainan Sousa <thainansousa7@gmail.com>
2023-08-05 14:15:57 -0300

    criado edição de professores

--- professores/urls.py
+++ professores/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('novo/', views.novo, name="novoProfessor"),
     path('gerenciar/', views.gerenciar, name="gerenciarProfessores"),
     path('cadastrar_professor',views.cadastrar_professor, name="cadastrarProfessor"),
-    path('alterar_status_professor/<int:id>', views.alterar_status_professor, name="alterarStatusProfessor")
+    path('alterar_status_professor/<int:id>', views.alterar_status_professor, name="alterarStatusProfessor"),
+    path('editar_professor/<int:id>', views.editar_professor, name="editarProfessor")
 ]
--- professores/views.py
+++ professores/views.py
@@ -5,10 +5,12 @@ from django.contrib import messages
 
 from .models import Professor
 
+from brutils import remove_symbols_cpf, is_valid_cpf, format_cpf
+
 def novo(request):
     if request.user.is_authenticated:
     
-        return render(request, 'novoProfessor.html')
+        return render(request, 'novoProfessor.html', {'edit': False})
     else:
         messages.add_message(request, constants.ERROR, 'Você precisa estar autenticado para acessar esta página.')
         return redirect('/')
@@ -20,8 +22,7 @@ def gerenciar(request):
         professor = request.GET.get('nome')
 
         if professor:
-            professor = professor.lower()
-            professores = Professor.objects.filter(nome=professor).order_by('-id')
+            professores = Professor.objects.filter(nome__iexact=professor).order_by('-id')
             return render(request, 'gerenciarProfessores.html', {'professores': professores})
         else:
             professores = Professor.objects.all().order_by('-id')
@@ -35,11 +36,11 @@ def cadastrar_professor(request):
     if request.user.is_authenticated:
 
         dados = {
-            'nome': request.POST.get('nome').lower(),
+            'nome': request.POST.get('nome'),
             'cpf': request.POST.get('cpf'),
             'registro': request.POST.get('registro'),
             'telefone': request.POST.get('telefone'),
-            'email': request.POST.get('email').lower(),
+            'email': request.POST.get('email'),
         }
 
         for dado in dados:
@@ -47,31 +48,52 @@ def cadastrar_professor(request):
                 messages.add_message(request, constants.ERROR, 'Preencha todos os campos!')
                 return redirect('/professores/novo')
             
-        emailExist = Professor.objects.filter(email=dados['email'])
+        emailExist = Professor.objects.filter(email__iexact=dados['email'])
         registroExist = Professor.objects.filter(registro=dados['registro'])
+        
+        cpfWithOutSymbols = remove_symbols_cpf(dados['cpf'])
 
-        if len(emailExist) == 1:
+        cpfIsValid = is_valid_cpf(cpfWithOutSymbols)
+
+        cpfFormated = format_cpf(cpfWithOutSymbols)
+
+        cpfExist = Professor.objects.filter(cpf=cpfFormated)
+
+        if not cpfIsValid:
+
+            messages.add_message(request, constants.ERROR, 'O CPF informado é invalido.')
+            return redirect('/professores/novo')
+        
+        elif len(emailExist) == 1:
+
             messages.add_message(request, constants.ERROR, 'O email informado já foi cadastrado.')
             return redirect('/professores/novo')
         
-        if len(registroExist) == 1:
+        elif len(registroExist) == 1:
+
             messages.add_message(request, constants.ERROR, 'O registro informado já foi cadastrado.')
             return redirect('/professores/novo')
-            
-
-        professor = Professor(
-            nome = dados['nome'],
-            cpf = dados['cpf'],
-            registro = dados['registro'],
-            telefone = dados['telefone'],
-            email = dados['email']
-        )
-
-        professor.save()
-
-        messages.add_message(request, constants.SUCCESS, f'Professor {dados["nome"]} cadastrado com sucesso!')
         
-        return redirect('/professores/novo')
+        elif len(cpfExist) == 1:
+
+            messages.add_message(request, constants.ERROR, 'O CPF informado já foi cadastrado.')
+            return redirect('/professores/novo')
+        
+        else:
+
+            professor = Professor(
+                nome = dados['nome'],
+                cpf = cpfFormated,
+                registro = dados['registro'],
+                telefone = dados['telefone'],
+                email = dados['email']
+            )
+
+            professor.save()
+
+            messages.add_message(request, constants.SUCCESS, f'Professor {dados["nome"]} cadastrado com sucesso!')
+            
+            return redirect('/professores/novo')
     else:
         messages.add_message(request, constants.ERROR, 'Você precisa estar autenticado para acessar esta página.')
         return redirect('/')
@@ -79,7 +101,6 @@ def cadastrar_professor(request):
 def alterar_status_professor(request, id):
 
     if request.user.is_authenticated:
-
 
         try:
             professor = Professor.objects.get(id=id)
@@ -97,6 +118,87 @@ def alterar_status_professor(request, id):
 
             messages.add_message(request, constants.ERROR, 'O professor informado não existe!')
         return redirect('/professores/gerenciar')
+    else:
+        messages.add_message(request, constants.ERROR, 'Você precisa estar autenticado para acessar esta página.')
+        return redirect('/')
+    
+def editar_professor(request, id):
+
+    if request.user.is_authenticated:
+        
+        if (request.method) == 'GET':
+            try:
+                professor = Professor.objects.get(id=id)
+                return render(request, 'novoProfessor.html', {'edit': True, 'professor': professor})
+            except Professor.DoesNotExist:
+                messages.add_message(request, constants.ERROR, 'O professor informado não existe.')
+                return redirect('/professores/gerenciar')
+        else:
+            try:
+                
+                professor = Professor.objects.get(id=id)
+
+                dados = {
+                    'nome': request.POST.get('nome'),
+                    'cpf': request.POST.get('cpf'),
+                    'registro': request.POST.get('registro'),
+                    'telefone': request.POST.get('telefone'),
+                    'email':request.POST.get('email')
+                }
+
+                for dado in dados:
+                    if len(dados[dado].strip()) == 0:
+                        messages.add_message(request, constants.ERROR, 'Preencha todos os campos.')
+                        return redirect(f'/professores/editar_professor/{professor.id}')
+
+                emailExist = Professor.objects.filter(email__iexact=dados['email'])
+                registroExist = Professor.objects.filter(registro=dados['registro'])
+
+                cpfWithOutSymbols = remove_symbols_cpf(dados['cpf'])
+
+                cpfIsValid = is_valid_cpf(cpfWithOutSymbols)
+
+                cpfFormated = format_cpf(cpfWithOutSymbols)
+
+                cpfExist = Professor.objects.filter(cpf=cpfFormated)
+
+                if not cpfIsValid:
+
+                    messages.add_message(request, constants.ERROR, 'O CPF informado não é valido.')
+                    return redirect(f'/professores/editar_professor/{professor.id}')
+                
+                elif emailExist and emailExist[0].id != professor.id:
+
+                    messages.add_message(request, constants.ERROR, 'O email informado já foi cadastrado.')
+                    return redirect(f'/professores/editar_professor/{professor.id}')
+                
+                elif registroExist and registroExist[0].id != professor.id:
+
+                    messages.add_message(request, constants.ERROR, 'O Nº de registro informado já foi cadastrado.')
+                    return redirect(f'/professores/editar_professor/{professor.id}')
+                
+                elif cpfExist and cpfExist[0].id != professor.id:
+
+                    messages.add_message(request, constants.ERROR, 'O CPF informado já foi cadastrado.')
+                    return redirect(f'/professores/editar_professor/{professor.id}')
+                
+                else:
+
+                    professor.nome = dados['nome']
+                    professor.cpf = cpfFormated
+                    professor.registro = dados['registro']
+                    professor.telefone = dados['telefone']
+                    professor.email = dados['email']
+
+                    professor.save()
+
+                    messages.add_message(request, constants.SUCCESS, f'O professor {professor.nome} foi editado com sucesso.')
+
+                    return redirect('/professores/gerenciar/')
+                
+            except Professor.DoesNotExist:
+                messages.add_message(request, constants.ERROR, 'O professor informado não existe.')
+                return redirect('/professores/gerenciar')
     else:
         messages.add_message(request, constants.ERROR, 'Você precisa estar autenticado para acessar esta página.')
         return redirect('/')
